d122e98b8cc6911a2c43560dd6783e60dfba24eb
Pavel Smolensky <pavelsmolensky@gmail.com>
2017-08-09 21:25:27 +0200

    Add InsertionSort

--- src/main/java/x/y/z/Sorting.java
+++ src/main/java/x/y/z/Sorting.java
@@ -16,7 +16,27 @@ public class Sorting {
         }
       }
     }
-    System.out.println("Elapsed time (ms): " + (System.currentTimeMillis() - start));
+    System.out.println("BubbleSort. Elapsed time (ms): " + (System.currentTimeMillis() - start));
+  }
+
+  public static <T extends Comparable<T>> void insertionSort(T[] array) {
+    long start = System.currentTimeMillis();
+    T temp;
+    if (array.length == 1) {
+      return;
+    }
+    for (int i = 1; i < array.length; i++) {
+      for (int j = i; j > 0; j--) {
+        if (array[j].compareTo(array[j - 1]) < 0) {
+          temp = array[j - 1];
+          array[j - 1] = array[j];
+          array[j] = temp;
+        } else {
+          break;
+        }
+      }
+    }
+    System.out.println("InsertionSort. Elapsed time (ms): " + (System.currentTimeMillis() - start));
   }
 
 }
---
+++ src/test/java/x/y/z/SortTest.java
@@ -0,0 +1,70 @@
+package x.y.z;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.function.Consumer;
+import java.util.stream.IntStream;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.ObjectFactory;
+import org.testng.annotations.Test;
+
+public class SortTest {
+  @Test(dataProvider = "sortings")
+  public void sortRandomArray(Consumer<Integer[]> sortingFunction) {
+    Integer[] toSort = createRandomIntArray(10);
+
+    Integer[] expected = Arrays.copyOf(toSort, toSort.length);
+    Arrays.sort(expected);
+
+    sortingFunction.accept(toSort);
+    Assert.assertEquals(expected, toSort);
+  }
+
+  @Test(dataProvider = "sortings")
+  public void sortEmptyArray(Consumer<Integer[]> sortingFunction) {
+    Integer[] toSort = new Integer[]{};
+    sortingFunction.accept(toSort);
+    Assert.assertEquals(toSort.length, 0);
+  }
+
+  @Test(dataProvider = "sortings")
+  public void sortOneElementArray(Consumer<Integer[]> sortingFunction) {
+    Integer[] toSort = new Integer[]{42};
+    sortingFunction.accept(toSort);
+    int first = toSort[0];
+    Assert.assertEquals(toSort.length, 1);
+    Assert.assertEquals(first, 42);
+  }
+
+  @Test(dataProvider = "sortings")
+  public void sortSameElementsArray(Consumer<Integer[]> sortingFunction) {
+    Integer[] toSort = new Integer[]{42, 42, 42};
+    sortingFunction.accept(toSort);
+    Assert.assertEquals(toSort, new Integer[]{42, 42, 42});
+  }
+
+  @Test(dataProvider = "sortings")
+  public void sortHugeArray(Consumer<Integer[]> sortingFunction) {
+    Integer[] toSort = createRandomIntArray(99999);
+
+    Integer[] expected = Arrays.copyOf(toSort, toSort.length);
+    Arrays.sort(expected);
+
+    sortingFunction.accept(toSort);
+    Assert.assertEquals(expected, toSort);
+  }
+
+  private Integer[] createRandomIntArray(int length) {
+    Integer[] result = new Integer[length];
+    Random r = new Random();
+    IntStream.range(0, length).forEach(i -> result[i] = r.nextInt());
+    return result;
+  }
+
+  @DataProvider
+  public Object[][] sortings() {
+    return new Object[][]{new Object[]{(Consumer<Integer[]>) Sorting::insertionSort}, {(Consumer<Integer[]>) Sorting::bubbleSort}};
+  }
+}
