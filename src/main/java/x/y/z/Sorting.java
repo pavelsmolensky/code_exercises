39de2d941106196ce033a8822ab60a13e2ba5847
Pavel Smalenski <pavelsmolensky@gmail.com>
2017-08-10 17:19:48 +0200

    Merge sort

--- src/main/java/x/y/z/Sorting.java
+++ src/main/java/x/y/z/Sorting.java
@@ -2,41 +2,84 @@ package x.y.z;
 
 public class Sorting {
 
-
-  public static <T extends Comparable<T>> void bubbleSort(T[] numArray) {
-    int n = numArray.length;
-    T temp;
-    long start = System.currentTimeMillis();
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n - i - 1; j++) {
-        if (numArray[j].compareTo(numArray[j + 1]) > 0) {
-          temp = numArray[j];
-          numArray[j] = numArray[j + 1];
-          numArray[j + 1] = temp;
-        }
+   public static <T extends Comparable<T>> void bubbleSort(T[] array) {
+      int n = array.length;
+      T temp;
+      long start = System.currentTimeMillis();
+      for (int i = 0; i < n; i++) {
+         for (int j = 0; j < n - i - 1; j++) {
+            if (array[j].compareTo(array[j + 1]) > 0) {
+               temp = array[j];
+               array[j] = array[j + 1];
+               array[j + 1] = temp;
+            }
+         }
       }
-    }
-    System.out.println("BubbleSort. Elapsed time (ms): " + (System.currentTimeMillis() - start));
-  }
+      System.out.println("BubbleSort. Elapsed time (ms): " + (System.currentTimeMillis() - start));
+   }
+   //-----------
 
-  public static <T extends Comparable<T>> void insertionSort(T[] array) {
-    long start = System.currentTimeMillis();
-    T temp;
-    if (array.length == 1) {
-      return;
-    }
-    for (int i = 1; i < array.length; i++) {
-      for (int j = i; j > 0; j--) {
-        if (array[j].compareTo(array[j - 1]) < 0) {
-          temp = array[j - 1];
-          array[j - 1] = array[j];
-          array[j] = temp;
-        } else {
-          break;
-        }
+   public static <T extends Comparable<T>> void insertionSort(T[] array) {
+      long start = System.currentTimeMillis();
+      T temp;
+      if (array.length == 1) {
+         return;
       }
-    }
-    System.out.println("InsertionSort. Elapsed time (ms): " + (System.currentTimeMillis() - start));
-  }
+      for (int i = 1; i < array.length; i++) {
+         for (int j = i; j > 0; j--) {
+            if (array[j].compareTo(array[j - 1]) < 0) {
+               temp = array[j - 1];
+               array[j - 1] = array[j];
+               array[j] = temp;
+            } else {
+               break;
+            }
+         }
+      }
+      System.out.println("InsertionSort. Elapsed time (ms): " + (System.currentTimeMillis() - start));
+   }
+   //-----------
+
+   static class MergeSort {
+      private static Comparable[] tempArray;
+
+      public static void mergeSort(Comparable[] array) {
+         long start = System.currentTimeMillis();
+         tempArray = new Comparable[array.length];
+         mergeSort(array, 0, array.length - 1);
+         System.out.println("MergeSort. Elapsed time (ms): " + (System.currentTimeMillis() - start));
+      }
+
+      private static void mergeSort(Comparable[] array, int low, int high) {
+         if (low < high) {
+            int mid = low + (high - low) / 2;
+            mergeSort(array, low, mid);
+            mergeSort(array, mid + 1, high);
+            merge(array, low, mid, high);
+         }
+      }
+
+      private static void merge(Comparable[] array, int low, int mid, int high) {
+         for (int i = low; i <= high; i++) {
+            tempArray[i] = array[i];
+         }
+
+         int leftIndex = low;
+         int rightIndex = mid + 1;
+
+         for (int k = low; k <= high; k++) {
+            if (leftIndex > mid) {
+               array[k] = tempArray[rightIndex++];
+            } else if (rightIndex > high) {
+               array[k] = tempArray[leftIndex++];
+            } else if (tempArray[leftIndex].compareTo(tempArray[rightIndex]) < 0) {
+               array[k] = tempArray[leftIndex++];
+            } else {
+               array[k] = tempArray[rightIndex++];
+            }
+         }
+
+      }
+   }
 
 }
--- src/test/java/x/y/z/SortTest.java
+++ src/test/java/x/y/z/SortTest.java
@@ -7,64 +7,67 @@ import java.util.stream.IntStream;
 
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.ObjectFactory;
 import org.testng.annotations.Test;
 
 public class SortTest {
-  @Test(dataProvider = "sortings")
-  public void sortRandomArray(Consumer<Integer[]> sortingFunction) {
-    Integer[] toSort = createRandomIntArray(10);
 
-    Integer[] expected = Arrays.copyOf(toSort, toSort.length);
-    Arrays.sort(expected);
+   @Test(dataProvider = "sortings")
+   public void sortRandomArray(Consumer<Integer[]> sortingFunction) {
+      Integer[] toSort = createRandomIntArray(10);
 
-    sortingFunction.accept(toSort);
-    Assert.assertEquals(expected, toSort);
-  }
+      Integer[] expected = Arrays.copyOf(toSort, toSort.length);
+      Arrays.sort(expected);
 
-  @Test(dataProvider = "sortings")
-  public void sortEmptyArray(Consumer<Integer[]> sortingFunction) {
-    Integer[] toSort = new Integer[]{};
-    sortingFunction.accept(toSort);
-    Assert.assertEquals(toSort.length, 0);
-  }
+      sortingFunction.accept(toSort);
+      Assert.assertEquals(expected, toSort);
+   }
 
-  @Test(dataProvider = "sortings")
-  public void sortOneElementArray(Consumer<Integer[]> sortingFunction) {
-    Integer[] toSort = new Integer[]{42};
-    sortingFunction.accept(toSort);
-    int first = toSort[0];
-    Assert.assertEquals(toSort.length, 1);
-    Assert.assertEquals(first, 42);
-  }
+   @Test(dataProvider = "sortings")
+   public void sortEmptyArray(Consumer<Integer[]> sortingFunction) {
+      Integer[] toSort = new Integer[] {};
+      sortingFunction.accept(toSort);
+      Assert.assertEquals(toSort.length, 0);
+   }
 
-  @Test(dataProvider = "sortings")
-  public void sortSameElementsArray(Consumer<Integer[]> sortingFunction) {
-    Integer[] toSort = new Integer[]{42, 42, 42};
-    sortingFunction.accept(toSort);
-    Assert.assertEquals(toSort, new Integer[]{42, 42, 42});
-  }
+   @Test(dataProvider = "sortings")
+   public void sortOneElementArray(Consumer<Integer[]> sortingFunction) {
+      Integer[] toSort = new Integer[] { 42 };
+      sortingFunction.accept(toSort);
+      int first = toSort[0];
+      Assert.assertEquals(toSort.length, 1);
+      Assert.assertEquals(first, 42);
+   }
 
-  @Test(dataProvider = "sortings")
-  public void sortHugeArray(Consumer<Integer[]> sortingFunction) {
-    Integer[] toSort = createRandomIntArray(99999);
+   @Test(dataProvider = "sortings")
+   public void sortSameElementsArray(Consumer<Integer[]> sortingFunction) {
+      Integer[] toSort = new Integer[] { 42, 42, 42 };
+      sortingFunction.accept(toSort);
+      Assert.assertEquals(toSort, new Integer[] { 42, 42, 42 });
+   }
 
-    Integer[] expected = Arrays.copyOf(toSort, toSort.length);
-    Arrays.sort(expected);
+   @Test(dataProvider = "sortings")
+   public void sortHugeArray(Consumer<Integer[]> sortingFunction) {
+      Integer[] toSort = createRandomIntArray(99999);
 
-    sortingFunction.accept(toSort);
-    Assert.assertEquals(expected, toSort);
-  }
+      Integer[] expected = Arrays.copyOf(toSort, toSort.length);
+      Arrays.sort(expected);
 
-  private Integer[] createRandomIntArray(int length) {
-    Integer[] result = new Integer[length];
-    Random r = new Random();
-    IntStream.range(0, length).forEach(i -> result[i] = r.nextInt());
-    return result;
-  }
+      sortingFunction.accept(toSort);
+      Assert.assertEquals(expected, toSort);
+   }
 
-  @DataProvider
-  public Object[][] sortings() {
-    return new Object[][]{new Object[]{(Consumer<Integer[]>) Sorting::insertionSort}, {(Consumer<Integer[]>) Sorting::bubbleSort}};
-  }
+   private Integer[] createRandomIntArray(int length) {
+      Integer[] result = new Integer[length];
+      Random r = new Random();
+      IntStream.range(0, length).forEach(i -> result[i] = r.nextInt());
+      return result;
+   }
+
+   @DataProvider
+   public Object[][] sortings() {
+      return new Object[][] {
+            { (Consumer<Integer[]>) Sorting::insertionSort },
+            { (Consumer<Integer[]>) Sorting::bubbleSort },
+            { (Consumer<Integer[]>) Sorting.MergeSort::mergeSort } };
+   }
 }
